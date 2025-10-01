14aa973c200a31c41af8610e095cbbe6937f2735
CARLOS ALBERTO BOLAÑOS GAMARRA <carlos.gamarraaaa@gmail.com>
2025-10-01 14:30:47 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Panel de administración cargado.");
+    
+    // Aquí puedes agregar interacciones, como eventos de clic en los botones o navegación
+});
