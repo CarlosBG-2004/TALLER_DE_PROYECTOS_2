2ba43db83064ed5542908085cd8efbc657a2e41a
CARLOS ALBERTO BOLAÑOS GAMARRA <carlos.gamarraaaa@gmail.com>
2025-10-08 22:15:32 -0500

    Delete PHP RAP TRAVEL S4 S5 directory
    para tener mas ordenado el programa y mas organizado

--- PHP RAP TRAVEL S4 S5/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log("Panel de administración cargado.");
-    
-    // Aquí puedes agregar interacciones, como eventos de clic en los botones o navegación
-});
